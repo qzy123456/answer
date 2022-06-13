44870da4c486999cdb376e8fa45d1087779aff4f
chaoyang <896773595@qq.com>
2022-06-13 19:50:01 +0800

    xx

--- server/common.go
+++ server/common.go
@@ -17,7 +17,7 @@ const (
 	pingPeriod                 = (pongWait * 9) / 10
 	messageSize                = 512
 	roomMaxUser                = 2
-	playGameTime time.Duration = 5 //每局游戏时间
+	playGameTime time.Duration = 15 //每局游戏时间
 )
 
 var (
